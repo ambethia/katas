8e1359d1a3a9674c43cc4fc20b4b97f81cb9f0ef
Jason L Perry <jasper@ambethia.com>
2017-08-05 23:34:11 -0400

    Refactoring

--- javascript/2-kyu/robby-the-robot/solution.js
+++ javascript/2-kyu/robby-the-robot/solution.js
@@ -1,81 +1,81 @@
+const DIRS = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+
 const getCommands = (field, power) => {
   const size = Math.sqrt(field.length)
   const goal = field.indexOf('T')
-  const [gx, gy] = [goal % size, (goal - goal % size) / size]
+  const gx = goal % size
+  const gy = (goal - gx) / size
+  const f = {}
+  const g = {}
+  const h = {}
+  const p = {}
   const nodes = {}
-
-  // Initialize the node graph
-  for (let index in field) {
-    const x = index % size
-    const y = (index - x) / size
-    const node = { x, y, neighbors: [], g: 0 }
-    // locate neighbors, excluding blocked cells
-    for (let [dx, dy] of [[1, 0], [0, 1], [-1, 0], [0, -1]]) {
-      const [nx, ny] = [x + dx, y + dy]
-      if (nx >= 0 && nx < size && ny >= 0 && ny < size) {
-        const ni = size * ny + nx
-        if (field[ni] !== '#') node.neighbors.push(ni)
-      }
-    }
-    // Pre-calculate the heuristics (estimated minimum cost) using
-    // Manhattan distance from the goal
-    const [dx, dy] = [x - gx, y - gy].map(Math.abs)
-    node.h = dx + dy
-    // Finally, save our node
-    nodes[index] = node
-  }
-
   // Implement A*
   const open = [field.indexOf('S')]
-  const closed = []
+  const closed = new Set()
   while (open[0] !== goal && open.length) {
     const current = open.shift()
-    closed.push(current)
-    const [cx, cy] = [nodes[current].x, nodes[current].y]
-    for (let n of nodes[current].neighbors) {
-      const [nx, ny] = [nodes[n].x, nodes[n].y]
-      // `move` might need to be based on Robby's rules, not Manhattan distance...
-      const move = [nx - cx, ny - cy].map(Math.abs).reduce((x, y) => x + y)
-      const cost = nodes[current].g + move
-      if (open.includes(n) && cost < nodes[n].g) open.splice(open.indexOf(n), 1)
-      if (closed.includes(n) && cost < nodes[n].g) closed.splice(open.indexOf(n), 1)
-      if (!open.includes(n) && !closed.includes(n)) {
-        nodes[n].g = cost
-        nodes[n].f = nodes[n].g + nodes[n].h
-        nodes[n].parent = current
-        open.push(n)
-        open.sort((a, b) => nodes[a].f - nodes[b].f)
+    closed.add(current)
+    const cx = current % size
+    const cy = (current - cx) / size
+    // locate neighbors, excluding blocked cells
+    for (let [dx, dy] of DIRS) {
+      const nx = cx + dx
+      const ny = cy + dy
+      if (nx >= 0 && nx < size && ny >= 0 && ny < size) {
+        const ni = size * ny + nx
+        // Compute and cache result of heuristic function (estimated
+        // minimum cost) using Manhattan distance from the goal
+        if (!h[ni]) h[ni] = Math.abs(nx - gx) + Math.abs(ny - gy)
+        if (field[ni] !== '#') {
+          // `move` might need to be based on Robby's rules, not Manhattan distance...
+          const move = Math.abs(nx - cx) + Math.abs(ny - cy)
+          const cost = g[current] + move
+          const inOpen = open.includes(ni)
+          const inClosed = closed.has(ni)
+          if (inOpen && cost < g[ni]) open.splice(open.indexOf(ni), 1)
+          if (inClosed && cost < g[ni]) closed.remove(ni)
+          if (!inOpen && !inClosed) {
+            g[ni] = cost
+            f[ni] = cost + h[ni]
+            p[ni] = current
+            open.push(ni)
+            open.sort((a, b) => f[a] - f[b])
+          }
+        }
       }
     }
   }
 
   // No path was found, we're done here.
-  if (!nodes[goal].parent) return []
+  if (!p[goal]) return []
 
   // Re-construct path from the goal back to start
   let path = []
   let next = goal
-  while (nodes[next]) {
+  while (field[next]) {
     path.push(next)
-    next = nodes[next].parent
+    next = p[next]
   }
-  path.reverse()
 
   // Build Robby's instruction from the path.
   // Robby starts by facing north
-  let [dx, dy] = [0, -1]
+  let dx = 0
+  let dy = -1
   let instructions = []
-  for (let i = 0; i < path.length - 1; i++) {
-    const [a, b] = path.slice(i, i + 2).map(n => nodes[n])
-    const [fx, fy] = [b.x - a.x, b.y - a.y]
+  for (let i = path.length - 1; i > 0; i--) {
+    const ax = path[i] % size
+    const ay = (path[i] - ax) / size
+    const bx = path[i - 1] % size
+    const by = (path[i - 1] - bx) / size
     const [rx, ry] = [-dy, dx]
     const [lx, ly] = [dy, -dx]
-    if (fx === dx && fy === dy) {
+    if (bx - ax === dx && by - ay === dy) {
       instructions.push('f')
-    } else if (b.x === lx + a.x && b.y === ly + a.y) {
+    } else if (bx === lx + ax && by === ly + ay) {
       ;[dx, dy] = [lx, ly]
       instructions.push('l', 'f')
-    } else if (b.x === rx + a.x && b.y === ry + a.y) {
+    } else if (bx === rx + ax && by === ry + ay) {
       ;[dx, dy] = [rx, ry]
       instructions.push('r', 'f')
     }
